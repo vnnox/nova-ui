aa7941d015a0816db543f24e8bc6f64fcec963b7
mengxiaowen <mengxw@novastar.tech>
2018-10-24 09:56:49 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "off",
+      "windows"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "no-useless-escape" : 0,
+    "no-console": 0
+  }
+};
