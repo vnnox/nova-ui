305280d674bdc25a5e41d7d4b11ba55b4b5221d9
smohan <smohan@163.com>
2019-05-09 17:38:59 +0800

    树组件优化

--- src/components/tree/template.js
+++ src/components/tree/template.js
@@ -1,16 +1,18 @@
 export const skeletonTpl = `
 <li role="treeitem" class="nv-tree__node<% if(expandAll || node.expanded) {%> tree-node--expanded<% } if(disabled || node.disabled) {%> nv-disabled<% } if(node.children && node.children.length) {%> tree-node--branch<% } else {%> tree-node--leaf<% } if(!node.visible) {%> tree-node--hidden<% }%>" data-node="<%= node.id%>">
   <div class="tree-node__inner" style="padding-left:<%= (node.level * indent)%>px;">
-    <span class="tree-node__icon<% if(!node.children || !node.children.length) {%> is-leaf<% }%>">
+    <span class="tree-node__fold tree-node__icon<% if(!node.children || !node.children.length) {%> is-leaf<% }%>">
       <i class="nv-icon-arrow-right"></i>
     </span> 
+    
     <% if(checkable) { %>
     <label class="nv-<%= chooseType %> tree-node__check" role="<%= chooseType%>">
       <input type="<%= chooseType%>" class="tree-node__value" name="<%= checkName %>" value="<%= node.id%>"<% if(disabled || node.disabled) {%> disabled<% } if (node.checked) {%> checked<% }%> />
       <i class="nv-<%= chooseType%>__icon"></i>
     </label>
     <% } %>
-    <div class="tree-node__label" title="<%= node.label%>"><%= node.content%></div>
+    <div class="tree-node__label" title="<%= node.label%>"><%= node.content%></div> 
+
   </div>
   <% if(node.children && node.children.length) {%>
     <ul role="group" class="tree-node__children">
@@ -19,6 +21,19 @@ export const skeletonTpl = `
   <%}%>  
 </li>
 `
+
+
+// 显示checkbox
+// 1. 关联label if checkbox && relate
+// <label><checkbox>text></label>
+// 2. 不关联label if checkbox && !relate
+// <label><checkbox></label> <text>
+// 不显示checkbox
+// if (!checkbox)
+// <text>
+
+
+
 
 export const noDataTpl = `
 <li class="nv-tree__empty"><%=noDataText%></li>
